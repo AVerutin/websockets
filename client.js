2dd4ec607f6191c50c9e7d91b573088aebc69069
AlexV <alexbroot@mail.ru>
2020-07-08 09:17:44 +0300

    Simple client-server chat

---
+++ client.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80;
+const host = 'localhost';
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.use('/', express.static(path.join(__dirname, '.')));
+
+app.listen(port, host, () => {
+    console.log('Server started on %s:%d', host, port);
+});
+
--- server.js
+++ server.js
@@ -5,13 +5,38 @@ const wsport = config.get('port');
 const wsServer = new WebSocketsServer.Server({port: wsport});
 
 var clients = [];
-wsServer.on ('connection', function(ws) {
+
+wsServer.on('connection', function connection(ws) {
     console.log('Connected on port %d', wsport);
-
+    var client = {};
     var id = clients.length;
-    clients[id] = ws;
-    console.log('Новое соединение #%d', id);
-    clients[id].send('Welcome! Your ID is %d', id);
-    
-});
 
+    client.ID = id;
+    client.Socket = ws;
+
+    clients.push(client);
+
+    console.log('Новое соединение #%d', id);
+    // clients[id].send(`Welcome! Your ID is ${id}`);
+
+    ws.on('message', function incoming(message) {
+        var now = new Date().toDateString();
+        var sndMessage = {};
+        var rcvMessage = JSON.parse(message);
+        sndMessage.login = rcvMessage.login;
+        sndMessage.text = rcvMessage.text;
+        sndMessage.date = now;
+        var txtMessage = `Message from ${rcvMessage.login}: ${rcvMessage.text}`;
+        console.log(txtMessage);
+        // Рассылаем ссобщение всем клиентам
+        for (let client of clients) {
+            if(client.ID != id) { 
+                client.Socket.send(JSON.stringify(sndMessage));
+            }
+        }
+    });
+
+    ws.on('close', function close() {
+        console.log('Connection closed');
+    })
+});
